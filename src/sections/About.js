c04e46a4d7bb517abde857e83931937de4a21f9f
Paolo Rollo <paolo.rollo1997@gmail.com>
2021-01-05 13:55:30 +0100

    update.

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,13 +1,31 @@
 import constants from '../constants';
+import me from '../assets/me.jpg';
+
+const description = "Welcome to my portfolio, I'm Paolo Rollo, a young software engineer based in Rome, Italy.\
+                    I'm interested in all the programming-related stuff, but also in topics like chemistry,\
+                    bioinformatics, cybersecurity and the universe. Before starting my university studies in \
+                    computer engineering my dream was to become a chemist of some sort, working with the smallest \
+                    particle of all - the atom - has always fascinated me: thinking like this, programming is not so \
+                    different - I may not be able to tinker with atoms, but I sure can with 0s and 1s."
 
 const About = () => {
     return (
-        <section id="about" style={{margin: "50px 0", paddingBottom: 50, borderBottom: "1px solid white"}}>
+        <section id="about" style={{paddingBottom: 50, borderBottom: "1px solid white"}}>
             <h1 style={{ fontSize: 50, width: '100%' }}>
                 about
                 <span className="primary"> me</span>
             </h1>
-            <p>{constants.loremIpsum}</p>
+            <div className="row" >
+                <div className="image-col">
+                    <figure>
+                        <img src={me} alt="very elevated picture" title="very elevated picture" />
+                        <figcaption>very elevated picture</figcaption>
+                    </figure>
+                </div>
+                <div className="text-col">
+                    <p style={{textAlign: 'justify'}}>{description}</p>
+                </div>
+            </div>
         </section>
     )
 }
--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,4 +1,99 @@
-import constants from '../constants';
+const skills = [
+    {
+        name: 'Programming languages',
+        data: [
+            { 
+                name: 'python',
+                percentage: '100%',
+            },
+            {
+                name: 'golang',
+                percentage: '90%'
+            },
+            {
+                name: 'javascript/typescript',
+                percentage: '95%'
+            },
+            {
+                name: 'java',
+                percentage: '80%'
+            },
+            {
+                name: 'rust',
+                extra: '(studying it)',
+                percentage: '50%'
+            },
+            {
+                name: 'haskell',
+                extra: '(studying it)',
+                percentage: '30%'
+            },
+            {
+                name: 'php',
+                extra: '(for real?)',
+                percentage: '0%'
+            }
+        ]
+    },
+    {
+        name: 'Not programming languages but still',
+        data: [
+            { 
+                name: 'nosql databases',
+                percentage: '100%',
+            },
+            { 
+                name: 'sql databases',
+                percentage: '95%',
+            },
+            { 
+                name: 'docker',
+                percentage: '100%',
+            },
+            { 
+                name: 'REST',
+                percentage: '100%',
+            },
+            { 
+                name: 'gRPC',
+                percentage: '90%',
+            },
+            {
+                name: 'ci/cd',
+                percentage: '90%',
+            },
+        ]
+    },
+    {
+        name: 'Soft skills',
+        data: [
+            {
+                name: 'Flexibility',
+                percentage: '90%',
+            },
+            {
+                name: 'Communication',
+                percentage: '85%',
+            },
+            {
+                name: 'Teamwork',
+                percentage: '100%',
+            },
+            {
+                name: 'Listening',
+                percentage: '85%',
+            },
+            {
+                name: 'Self-management',
+                percentage: '80%',
+            },
+            {
+                name: 'Divergent thinking',
+                percentage: '100%',
+            }
+        ]
+    }
+];
 
 const Skills = () => {
     return (
@@ -7,7 +102,27 @@ const Skills = () => {
                 personal
                 <span className="primary"> skills</span>
             </h1>
-            <p>{constants.loremIpsum}</p>
+            {
+                skills.map((skill, i) => {
+                    return (
+                        <div style={{borderBottom: `${i === skills.length - 1 ? "" : '1px dashed white'}`, marginBottom: 20}}>
+                            <h2>{skill.name}</h2>
+                            { skill.data.map((item, j) => {
+                                return (
+                                    <>
+                                    <p style={{marginBottom: 0, marginTop: 25}}>{item.name}{item.extra ? <span style={{float: 'right'}}>{item.extra}</span> : <></>}</p>
+                                    <div className={`progress-bar ${item.name === 'php' ? "progress-bar-no-arrow" : "progress-bar-show-percent"}`}>
+                                        <div className="progress-bar-filled" style={{width: item.percentage}} data-filled={item.name !== 'php' ? item.percentage : ''}></div>
+                                    </div>
+                                    </>
+                                )
+                            })}
+                            <br/>
+                        </div>
+                    )
+                })
+            }
+            
         </section>
     )
 }
